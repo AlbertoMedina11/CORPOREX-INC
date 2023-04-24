0823e17ec54114a8ed85a1ba552187406a132644
AlbertoMedina11 <alberto.medina.gardea@gmail.com>
2023-04-24 09:43:05 -0500

    se agrega html, copor, contratos, intrumentos, y css

---
+++ store/dummy.js
@@ -0,0 +1,31 @@
+const db = {
+    'user': [
+        { id: '2' , name: 'albertito'},
+    ]
+};
+
+async function list(tabla){
+    return db[tabla];
+};
+
+async function get(tabla, id){
+    let col = await list(tabla);
+    return col.filter(item => item.id === id)[0] || null;
+};
+
+
+async function upsert(tabla, data){
+    db[collection].push(data);
+};
+
+function remove(tabla, id){
+    return true;
+
+};
+
+module.exports = {
+    list,
+    get,
+    upsert,
+    remove,
+};
