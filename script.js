d92107e65b7a2a8b541e0ab0bdd3b1a24c46e9b2
AlbertoMedina11 <alberto.medina.gardea@gmail.com>
2023-05-23 11:09:10 -0500

    se agrega nueva info

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 
 // PORTADA HTML
 
-const { Alert } = require("bootstrap");
-const { log } = require("console");
-
 const input1 = document.querySelector('#inputid1');
 
 const input2 = document.querySelector('#inputid2');
+
 
 
 const nuevohtml = document.querySelector('#nuevohtml');
@@ -17,16 +15,25 @@ const ingreso1 = document.querySelector('#ingreso1');
 const ingreso2 = document.querySelector('#ingreso2');
 const ingreso3 = document.querySelector('#ingreso3');
 const ingreso4 = document.querySelector('#ingreso4');
-const ingreso5 = document.querySelector('#ingreso5');
+
+
+
+var boton = document.createElement('button');
+boton.innerText = 'this button';
+
+
+
 
 function btnOnClick(){
     const nombre = input1.value;
     const razon = input2.value;
   
-
+    
+    
     ingreso1.innerText = nombre;
-    ingreso2.innerText = "Razon o Denominacion Social" + razon;
-
+    ingreso2.innerText = "Razon o Denominacion Social: " + razon  ;   
+    appendChild(boton)
+    
 };
 
 // PATITOS 
@@ -34,7 +41,7 @@ function btnOnClick(){
 function Escoger(){
     const option = document.getElementById('Escoger-contratos').value;
 
-    if (option === "Arrendamiento " ){
+    if (option == "Arrendamiento " ){
         windows.location.href= "./html/arrendamiento.html"
     } else if (option === "Compra venta"){
         windows.location.href= "./html/compraventa.html"
@@ -42,5 +49,3 @@ function Escoger(){
     
 };
 
-
-export default btnOnClick;
