bf08a158422cfa059e4a128ab711266c6ceecca0
AlbertoMedina11 <alberto.medina.gardea@gmail.com>
2023-05-24 17:02:50 -0500

    se modifico los htmls

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 
 // PORTADA HTML
 
+
+
+
 const input1 = document.querySelector('#inputid1');
 
 const input2 = document.querySelector('#inputid2');
@@ -15,25 +18,36 @@ const ingreso1 = document.querySelector('#ingreso1');
 const ingreso2 = document.querySelector('#ingreso2');
 const ingreso3 = document.querySelector('#ingreso3');
 const ingreso4 = document.querySelector('#ingreso4');
-
-
-
-var boton = document.createElement('button');
-boton.innerText = 'this button';
-
-
+const ingreso5= document.querySelector('#ingreso5');
+const ingreso6 = docuement.querySelector('#ingreso6');
 
 
 function btnOnClick(){
     const nombre = input1.value;
     const razon = input2.value;
-  
-    
-    
+    const nuevoboton = document.createElement('button');
+   
+
+
     ingreso1.innerText = nombre;
-    ingreso2.innerText = "Razon o Denominacion Social: " + razon  ;   
-    appendChild(boton)
-    
+    ingreso2.innerText = "Razon o Denominacion Social: " + razon  ; 
+    ingreso5.appendChild(nuevoboton);  
+};
+
+
+function Borrador1(){
+    const caja1 = document.querySelector('div.col-3')
+    caja1.remove(caja1);
+};
+
+function Borrador2(){
+    const caja2 = document.querySelector('div.col-2')
+    caja2.remove(caja2);
+};
+
+function Borrador3(){
+    const caja3 = document.querySelector('div.col-1')
+    caja3.remove(caja3);
 };
 
 // PATITOS 
